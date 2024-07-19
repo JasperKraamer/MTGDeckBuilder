c6a350df9a5886ef74425ff1036b05017ad8391c
JasperKraamer <jasperkraamer@gmail.com>
2024-07-19 13:01:53 +0200

    added styling to navigationbar

--- src/Components/Navigation/NavigationBar.jsx
+++ src/Components/Navigation/NavigationBar.jsx
@@ -5,25 +5,25 @@ function NavigationBar() {
     return (
         <>
             <nav>
-                <ul>
-                    <li>
-                        MTG Deck Builder
+                <ul className="navBar1">
+                    <li className="appName">
+                        MTG Deckmaster
                     </li>
                 </ul>
-                <ul>
-                    <li>
+                <ul className="navBar2">
+                    <li className="button1">
                         Search your card
                     </li>
-                    <li>
+                    <li className="button1">
                         Sign in
                     </li>
-                    <li>
+                    <li className="button1">
                         Sign up
                     </li>
-                    <li>
+                    <li className="button1">
                         Profile
                     </li>
-                    <li>
+                    <li className="button1">
                         Log out
                     </li>
                 </ul>
