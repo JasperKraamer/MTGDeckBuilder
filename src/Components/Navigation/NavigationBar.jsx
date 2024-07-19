f67c727740fc6643ae4dbccadbc3fdee490043cb
JasperKraamer <jasperkraamer@gmail.com>
2024-07-19 12:14:56 +0200

    added component navigationBar

---
+++ src/Components/Navigation/NavigationBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavigationBar.css';
+
+function NavigationBar() {
+    return (
+        <>
+            <p>Welcome on the first component</p>
+        </>
+    )
+}
+
+export default NavigationBar;
