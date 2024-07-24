9eccc1c3e4935b85558fc4b663d107df9f244094
JasperKraamer <jasperkraamer@gmail.com>
2024-07-24 08:46:14 +0200

    basic login and styling

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,8 +1,21 @@
+import'./SignIn.css'
+
 function SignIn() {
     return (
-        <>
-            <h2>Sign in here</h2>
-        </>
+        <div className="background">
+            <div className="form-container">
+                <h2>Log In</h2>
+                <form>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" required/>
+
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" required/>
+
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
