6cf4af6868b2f9b79e8943462564035734f9d298
JasperKraamer <jasperkraamer@gmail.com>
2024-07-25 11:44:17 +0200

    footer and teaser added and functional

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import SignUp from "./Pages/SignUp/SignUp.jsx";
 import Profile from "./Pages/Profile/Profile.jsx";
 import NotFound from "./Pages/NotFound/NotFound.jsx";
 import Search from "./Pages/Search/Search.jsx";
+import Teaser from "./Components/Teaser/Teaser.jsx"
+import Footer from "./Components/Footer/Footer.jsx"
 
 function App() {
     return (
     <>
         <NavigationBar />
+        <Teaser />
 
         <Routes>
             <Route path="/" element={<Home/>}/>
@@ -21,6 +24,8 @@ function App() {
             <Route path="/search" element={<Search/>}/>
             <Route path="/*" element={<NotFound/>}/>
         </Routes>
+
+        <Footer />
     </>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+function Footer() {
+    return (
+        <div className="Footer-bar">
+            <p className="Footer-font">© 2024 In opdracht van NOVI Hogeschool gemaakt door Jasper Kraamer</p>
+        </div>
+    )
+}
+
+export default  Footer;
