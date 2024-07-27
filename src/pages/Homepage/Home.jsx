0a2f9d87b76f662b831b1f8e8d7ea2361709f7a4
JasperKraamer <jasperkraamer@gmail.com>
2024-07-27 12:28:26 +0200

    home page asides

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -1,7 +1,13 @@
 function Home() {
     return (
-        <main>
-                <h2>Home pagina</h2>
+        <main className="background">
+            <div className="fullscreen">
+            <section className="text-left">The latest news about Magic the Gathering</section>
+            <section className="text-right">
+                <h1 className="about">About this application</h1>
+                With this application you will be able to find all avalible magic the gathering cards.
+                when you found a card you like you will have the option to save the card as favorite or you can start building your own deck. when you  build your deck you can save  up to 60 cards. after you build your deck you can save it to your profile or you can export your deck to a pdf file so you can go to the store to buy the missing  cards. </section>
+            </div>
         </main>
 )
 }
