7a94e9159a399027b8ac485e8b1701e00aea11e6
JasperKraamer <jasperkraamer@gmail.com>
2024-09-02 13:39:55 +0200

    fixt typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css'
 import NavigationBar from "./Components/Navigation/NavigationBar.jsx";
-import SignIn from "./Pages/SignIn/SignIn.jsx";
-import Home from "./Pages/Homepage/Home.jsx";
-import SignUp from "./Pages/SignUp/SignUp.jsx";
-import Profile from "./Pages/Profile/Profile.jsx";
-import NotFound from "./Pages/NotFound/NotFound.jsx";
-import Search from "./Pages/Search/Search.jsx";
+import SignIn from "./pages/SignIn/SignIn.jsx";
+import Home from "./pages/Homepage/Home.jsx";
+import SignUp from "./pages/SignUp/SignUp.jsx";
+import Profile from "./pages/Profile/Profile.jsx";
+import NotFound from "./pages/NotFound/NotFound.jsx";
+import Search from "./pages/Search/Search.jsx";
 import Teaser from "./Components/Teaser/Teaser.jsx"
 import Footer from "./Components/Footer/Footer.jsx"
 import PrivateRoute from "./Components/PrivateRoute.jsx";
